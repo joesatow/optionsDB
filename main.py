664542eca1a0c947eda9ab43412789b348dfa053
joesatow <joseph.satow@gmail.com>
2023-03-11 15:02:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from datetime import datetime, timedelta
 from helper_funcs.stockList import getSymbols
+from tqdm import tqdm
 import os
 import requests
 import json
@@ -24,7 +25,7 @@ friday = today + timedelta( (4-today.weekday()) % 7 )
 endDate = (friday + timedelta(days=35)).strftime('%Y-%m-%d')
 endDate = '2023-03-17'
 insertStatement = "INSERT INTO `todaysDate` (`putCall`, `symbol`, `description`, `bid`, `ask`, `last`, `mark`, `openInterest`) VALUES "
-strikeCount = 100
+strikeCount = 1
 
 for symbol in stockList:
     url = f"https://api.tdameritrade.com/v1/marketdata/chains?apikey={tdAPIkey}&symbol={symbol}&strikeCount={strikeCount}&fromDate=2022-10-10&toDate={endDate}"
@@ -33,24 +34,26 @@ for symbol in stockList:
     headers = {}
 
     response = requests.request("GET", url, headers=headers, data=payload).json()
-
+    if response['status'] == 'FAILED':
+        print("error: " + response)
+        break
     for currentMap in response:
         if currentMap != 'callExpDateMap' and currentMap != 'putExpDateMap':
             continue
         for expDate in response[currentMap]:
             for strike in response[currentMap][expDate]:
-                for contract in response[currentMap][expDate][strike]:
-                    #print(json.dumps(contract, indent=2))
-                    putCall = contract['putCall']
-                    contractSymbol = contract['symbol']
-                    description = contract['description']
-                    bid = contract['bid']
-                    ask = contract['ask']
-                    last = contract['ask']
-                    mark = contract['mark']
-                    openInterest = contract['openInterest']
+                contract = response[currentMap][expDate][strike][0]
+                #print(json.dumps(contract, indent=2))
+                putCall = contract['putCall']
+                contractSymbol = contract['symbol']
+                description = contract['description']
+                bid = contract['bid']
+                ask = contract['ask']
+                last = contract['ask']
+                mark = contract['mark']
+                openInterest = contract['openInterest']
 
-                    insertStatement += f"('{putCall}', '{contractSymbol}', '{description}', '{bid}', '{ask}', '{last}', '{mark}', '{openInterest}'), "
+                insertStatement += f"('{putCall}', '{contractSymbol}', '{description}', '{bid}', '{ask}', '{last}', '{mark}', '{openInterest}'), "
 
     print(symbol + ' done...')                
 
